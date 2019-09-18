db4fd11a4ed6eb88924162049f67c79cb0ecd1dd
Lucas Santana <lvcs@cin.ufpe.br>
2019-09-17 21:55:13 -0300

    Finish step 2

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,23 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        downloadXml("https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml")
+    }
+
+    fun downloadXml(url: String) {
+        doAsync {
+            val xml = URL(url).readText()
+            val itemList = Parser.parse(xml)
+
+        }
     }
 }
